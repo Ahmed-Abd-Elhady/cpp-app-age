24b4632884a49ae3fd0d8eae8ae15b4ca24dd956
Ahmed Abd-Elhady|コナファ <111198618+Ahmed-Abd-Elhady@users.noreply.github.com>
2023-11-12 18:42:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,45 @@
 #include <iostream>
 using namespace std;
-#define days 9
-int main()
+
+int user_age;
+int user_choose;
+
+void showcase()
+{
+  cout << "--------------------------\n";
+  cout << "-Your Age Applection-\n";
+  cout << "--------------------------\n";
+  cout << "1-Calc Age : \n";
+  cout << "2-Exit : \n";
+  cin >> user_choose;
+}
+
+void cal()
 {
 
-    int user_age;
-    cout << "--------------------------\n";
-    cout << "-Your Age Applection-\n";
-    cout << "--------------------------\n";
-    cout << "Write your age here sir : \n";
-    cin >> user_age;
-    cout << "================================\n";
-    cout << "You lived : " << user_age * 365 / 12 << " mounths\n";
-    cout << "You lived : " << user_age * 365 / 7 << " weeks\n";
-    cout << "You lived : " << user_age * 365 << " days\n";
-    cout << "You lived : " << user_age * 365 * 24 << " hourts\n";
-    cout << "You lived : " << user_age * 365 * 24 * 60 << " min\n";
-    cout << "You lived : " << user_age * 365 * 24 * 60 * 60 << " secounds\n";
-    cout << "================================\n";
+  do
+  {
+    showcase();
+    switch (user_choose)
+    {
+    case 1:
+      cout << "Enter your age here sir : \n";
+      cin >> user_age;
+      cout << "================================\n";
+      cout << "You lived : " << user_age * 365 / 12 << " mounths\n";
+      cout << "You lived : " << user_age * 365 / 7 << " weeks\n";
+      cout << "You lived : " << user_age * 365 << " days\n";
+      cout << "You lived : " << user_age * 365 * 24 << " hourts\n";
+      cout << "You lived : " << user_age * 365 * 24 * 60 << " min\n";
+      cout << "You lived : " << user_age * 365 * 24 * 60 * 60 << " secounds\n";
+      cout << "================================\n";
+      cal();
+      break;
+    }
+  } while (user_choose < 1);
+}
+
+int main()
+{
+  cal();
 }
